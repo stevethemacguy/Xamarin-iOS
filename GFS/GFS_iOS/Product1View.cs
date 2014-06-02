1c87f6616089ccff04a64ac63d7baecb582c6326
Steven Dunn <sdunn@siteworx.com>
2014-06-02 14:21:04 -0700

    Changed Text Field to a Button

---
+++ GFS/GFS_iOS/Product1View.cs
@@ -0,0 +1,14 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace GFS_iOS
+{
+	partial class Product1View : UIViewController
+	{
+		public Product1View (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
---
+++ GFS/GFS_iOS/Product2View.cs
@@ -0,0 +1,14 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace GFS_iOS
+{
+	partial class Product2View : UIViewController
+	{
+		public Product2View (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
