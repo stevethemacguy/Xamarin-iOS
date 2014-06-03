21cb89928669e76ad73554cbe708491b67019692
Steven Dunn <sdunn@siteworx.com>
2014-06-03 15:55:04 -0700

    Created catalog header cell.

---
+++ GFS/GFS_iOS/ProductActionsTableController.cs
@@ -0,0 +1,14 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace GFS_iOS
+{
+	partial class ProductActionsTableController : UITableViewController
+	{
+		public ProductActionsTableController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
--- GFS/GFS_iOS/ProductDetailsTableController.designer.cs
+++ GFS/GFS_iOS/ProductDetailsTableController.designer.cs
@@ -14,8 +14,40 @@ namespace GFS_iOS
 	[Register ("ProductDetailsTableController")]
 	partial class ProductDetailsTableController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIImageView CellBorder1 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIImageView CellBorder4 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITableViewCell Result1 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITableViewCell Result5 { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (CellBorder1 != null) {
+				CellBorder1.Dispose ();
+				CellBorder1 = null;
+			}
+			if (CellBorder4 != null) {
+				CellBorder4.Dispose ();
+				CellBorder4 = null;
+			}
+			if (Result1 != null) {
+				Result1.Dispose ();
+				Result1 = null;
+			}
+			if (Result5 != null) {
+				Result5.Dispose ();
+				Result5 = null;
+			}
 		}
 	}
 }
