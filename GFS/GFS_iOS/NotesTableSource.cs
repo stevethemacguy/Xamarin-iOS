006cd70ca4f088129830b8ff38eec680affd8bdf
Steven Dunn <sdunn@siteworx.com>
2014-06-06 16:04:05 -0700

    When a table row is selected, the index is printed out to the console.

---
+++ GFS/GFS_iOS/NotesTableSource.cs
@@ -0,0 +1,42 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+
+namespace GFS_iOS
+{
+	public class NotesTableSource : UITableViewSource {
+		protected string[] tableItems;
+		protected string cellIdentifier = "TableCell";
+		public NotesTableSource (string[] items)
+		{
+			tableItems = items;
+		}
+
+		public override int RowsInSection (UITableView tableview, int section)
+		{
+			return tableItems.Length;
+		}
+
+		//When the row is clicked, segue to the note view.
+		public override void RowSelected (UITableView tableView, NSIndexPath indexPath)
+		{
+			//tableItems[indexPath] the actual text of the row
+			Console.WriteLine(indexPath.Row); //the row number
+			tableView.DeselectRow (indexPath, true); // iOS convention is to remove the highlight
+		}
+
+		public override UITableViewCell GetCell (UITableView tableView, MonoTouch.Foundation.NSIndexPath indexPath)
+		{
+			// request a recycled cell to save memory
+			UITableViewCell cell = tableView.DequeueReusableCell (cellIdentifier);
+			// if there are no cells to reuse, create a new one
+			if (cell == null)
+				cell = new UITableViewCell (UITableViewCellStyle.Default, cellIdentifier);
+			cell.TextLabel.Text = tableItems[indexPath.Row];
+			return cell;
+		}
+	}
+
+}
