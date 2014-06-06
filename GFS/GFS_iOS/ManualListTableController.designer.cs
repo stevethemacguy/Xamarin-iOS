34372ed6ae6181c9f0d56a206fda642a81ed597c
Steven Dunn <sdunn@siteworx.com>
2014-06-06 12:13:55 -0700

    Changed border lines for PDFs back to iPhone defaults instead of blue border.

--- GFS/GFS_iOS/ManualListTableController.designer.cs
+++ GFS/GFS_iOS/ManualListTableController.designer.cs
@@ -28,10 +28,6 @@ namespace GFS_iOS
 
 		[Outlet]
 		[GeneratedCode ("iOS Designer", "1.0")]
-		MonoTouch.UIKit.UIImageView pdfImage { get; set; }
-
-		[Outlet]
-		[GeneratedCode ("iOS Designer", "1.0")]
 		MonoTouch.UIKit.UILabel ProductManual { get; set; }
 
 		[Outlet]
@@ -51,10 +47,6 @@ namespace GFS_iOS
 			if (ManualListView != null) {
 				ManualListView.Dispose ();
 				ManualListView = null;
-			}
-			if (pdfImage != null) {
-				pdfImage.Dispose ();
-				pdfImage = null;
 			}
 			if (ProductManual != null) {
 				ProductManual.Dispose ();
