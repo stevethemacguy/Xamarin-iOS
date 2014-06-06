2cd72433d69222a0e550694678344dd55e08c17b
Steven Dunn <sdunn@siteworx.com>
2014-06-06 12:06:34 -0700

    Changed buttons to labels so that selecting a cell works properly.

--- GFS/GFS_iOS/ProductPageViewController.cs
+++ GFS/GFS_iOS/ProductPageViewController.cs
@@ -35,26 +35,6 @@ namespace GFS_iOS
 			scrollView.ContentSize = new SizeF(226, 735); 
 			scrollView.AddSubview (imageView);
 			scrollView.Bounces = false; //We don't want it to "bounce" when it reaches the bottom.
-
-
-
-//			//Create a scroll view. X, Y, Width, Height. Note that the height MUST be smaller than the image if you want it to scroll!
-//			scrollView = new UIScrollView (new RectangleF(5, 380, 320, 200));
-//			View.AddSubview (scrollView);
-//
-//			//Create the ImageView
-//			imageView = new UIImageView (UIImage.FromFile ("description-text.png"));
-//
-//			//Make the image frame's width smaller than the Image width (of 320) to make (shrink) the image smaller
-//			imageView.Frame = new RectangleF(0,0,220, 430);
-//			imageView.ContentMode = UIViewContentMode.ScaleAspectFit;
-//
-//			//Match the widht/height to (roughly) the frame dimensions so you can't scroll horizontal and can't scroll to far vertically.
-//			scrollView.ContentSize = new SizeF(220, 460); 
-//			scrollView.AddSubview (imageView);
-//			scrollView.Bounces = false; //We don't want it to "bounce" when it reaches the bottom.
-//
-
 		}
 	}
 }
