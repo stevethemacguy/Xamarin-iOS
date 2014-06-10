32b4d3685545c8420bd2d14fd33e5fda21f4100d
Steven Dunn <sdunn@siteworx.com>
2014-06-10 14:12:51 -0700

    Save button correctly saves the list. TextInput now has focus on load and keyboard dismisses when pressing the done button.

--- GFS/GFS_iOS/TextInputController.cs
+++ GFS/GFS_iOS/TextInputController.cs
@@ -11,12 +11,38 @@ namespace GFS_iOS
 		public TextInputController (IntPtr handle) : base (handle)
 		{
 		}
+
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
+			ListInputField.BecomeFirstResponder(); //Put the users cursor in the text field
+
+			//Dismiss the keyboard when the enter key is pressed.
+			ListInputField.ShouldReturn = delegate
+			{
+				ListInputField.ResignFirstResponder();
+				return true;
+			};
 
 			//When the Save button is pressed, create a new list with the text.
+			CreateListButton.TouchUpInside += (o,s) => {
+				string newList = ListInputField.Text; //get the current text
 
+				//Don't allow blank notes to be created
+				if(newList == "")
+				{
+					UIAlertView alert = new UIAlertView ("Failed to Save", "Please enter a name for the new saved list.", null, "OK");
+					alert.Show();
+					return; //Do not segue. Keep the user here until they fix the problem or back out of the note.
+				}
+				else{
+					//create the list
+					DataSource source = DataSource.getInstance();
+					source.addSavedList(newList);
+					//Now transition back to the product page and display an alert saying that it was saved.
+				}
+
+			};
 
 			//*****Create a Save BAR BUTTON instead of the text view and use it to save notes*****
 			//Create the Save button and add it to the toolbar
