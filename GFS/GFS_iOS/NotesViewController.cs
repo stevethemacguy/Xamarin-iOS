890c8ca020ca5fa1bae21ab7f8276f9244893dd0
Steven Dunn <sdunn@siteworx.com>
2014-06-10 19:00:20 -0700

    Notes now use back button to save instead of the Save Menu item.

--- GFS/GFS_iOS/NotesViewController.cs
+++ GFS/GFS_iOS/NotesViewController.cs
@@ -26,9 +26,46 @@ namespace GFS_iOS
             myController = this;
 		}
 
+		private void SaveNote()
+		{
+			noteText = NoteTextView.Text; //get the current text
+			//Don't allow blank notes to be created
+			if(noteText == "")
+			{
+				return; //Do NOT create the note
+			}
+			if(addingNote)
+			{
+				notes.Add(noteText); //add a new note
+			}
+			else
+			{
+				notes[index] = noteText; //Overrite the existing note Text of the clicked cell to the new text
+			}
+			tableController.refreshTable(notes, noteText); //"Refresh" the table using our new list of notes.
+		}
+
+		//When the back button is pressed
+		public override void ViewWillDisappear(bool animated)
+		{
+			base.ViewWillDisappear(animated);
+			SaveNote();
+		}
+
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
+
+			//If we're adding a new note, make the text view empty.
+			if (addingNote)
+			{
+				NoteTextView.Text = "";
+			}
+			else //Otherwise, use the existing note text
+			{
+				NoteTextView.Text = notes[index]; //Set the current note text to the Note text passed in (the text that corresponds with the cell that was clicked).
+				addingNote = false; //We're editing an existing note, not adding one.
+			}
 
 			//Add camera icon to the toolbar
 			UIToolbar toolbar = new UIToolbar(new RectangleF(0.0f, 0.0f, this.View.Frame.Size.Width, 31.0f));
@@ -43,49 +80,20 @@ namespace GFS_iOS
 					this.NoteTextView.ResignFirstResponder();
 				})
 			};
-				
+
 			//this.NoteTextView.KeyboardAppearance = UIKeyboardAppearance.Dark;
-            this.NoteTextView.InputAccessoryView = toolbar;
+			this.NoteTextView.InputAccessoryView = toolbar;
 
-			//If we're adding a new note, make the text view empty.
-			if (addingNote)
-			{
-				NoteTextView.Text = "";
-			}
-			else //Otherwise, use the existing note text
-			{
-				NoteTextView.Text = notes[index]; //Set the current note text to the Note text passed in (the text that corresponds with the cell that was clicked).
-				addingNote = false; //We're editing an existing note, not adding one.
-			}
-
+			//******** USE THE TOOLBAR "SAVE" BUTTON******/
 			//Create the Save button and add it to the toolbar
-			UIBarButtonItem SavedNoteButton = new UIBarButtonItem();
-			SavedNoteButton.Title = "Save";
-			this.NavigationItem.SetRightBarButtonItem(SavedNoteButton, false);
-
-			//When the Save button is pressed, save the text.
-			SavedNoteButton.Clicked += (o,s) => {
-				noteText = NoteTextView.Text; //get the current text
-
-				//Don't allow blank notes to be created
-				if(noteText == "")
-				{
-					UIAlertView alert = new UIAlertView ("Failed to Save", "You can not save an empty note.", null, "OK");
-					alert.Show();
-					return; //Do not segue. Keep the user here until they fix the problem or back out of the note.
-				}
-
-				if(addingNote)
-				{
-					notes.Add(noteText); //add a new note
-				}
-				else
-				{
-					notes[index] = noteText; //Overrite the existing note Text of the clicked cell to the new text
-				}
-
-				tableController.refreshTable(notes, noteText); //"Refresh" the table using our new list of notes.
-			};
+//			UIBarButtonItem SavedNoteButton = new UIBarButtonItem();
+//			SavedNoteButton.Title = "Save";
+//			this.NavigationItem.SetRightBarButtonItem(SavedNoteButton, false);
+//
+//			//When the Save button is pressed, save the text.
+//			SavedNoteButton.Clicked += (o,s) => {
+//				SaveNote();
+//			};
 		}
         
         private void imagePicker(object sender, EventArgs e)
