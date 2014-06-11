32fa6e74f15faa125bceaee248093f3700e52123
jason10214 <jachen@siteworx.com>
2014-06-11 11:54:52 -0700

    home page scrollviews implemented

--- GFS/GFS_iOS/GFS_iOSViewController.cs
+++ GFS/GFS_iOS/GFS_iOSViewController.cs
@@ -8,6 +8,10 @@ namespace GFS_iOS
 {
 	public partial class GFS_iOSViewController : UIViewController
 	{
+        UIScrollView scrollView;
+        UIImageView imageView;
+        UIScrollView mainScrollView;
+
 		public GFS_iOSViewController(IntPtr handle) : base(handle)
 		{
 		}
@@ -25,7 +29,38 @@ namespace GFS_iOS
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
-			
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view, typically from a nib.   
+
+            mainScrollView = new UIScrollView(
+            new RectangleF(0, 64, 320
+            , 504));
+            RectangleF container = new RectangleF(0, 0, 320, 800);
+            mainScrollView.ContentSize = container.Size;
+
+            scrollView = new UIScrollView(
+            new RectangleF(0, 207, View.Frame.Width
+            , 150));
+
+            imageView = new UIImageView(UIImage.FromFile("itemlist150.png"));
+            //imageView.Frame = new RectangleF(0, 0, 600, 130);
+            //scrollView.ContentSize = imageView.Frame.Size;
+            scrollView.ContentSize = imageView.Image.Size;
+            scrollView.AddSubview(imageView);
+
+            //mainScrollView.AddSubview(scrollView);
+            View.AddSubview(mainScrollView);
+
+            imageView = new UIImageView(UIImage.FromFile("homeadd.png"));
+            mainScrollView.AddSubview(imageView);
+            imageView = new UIImageView(UIImage.FromFile("recommandtxt.png"));
+            imageView.Frame = new RectangleF(0, 166, 320, 41);
+            mainScrollView.AddSubview(imageView);
+            imageView = new UIImageView(UIImage.FromFile("viewed.png"));
+            imageView.Frame = new RectangleF(0, 357, 320, 350);
+            mainScrollView.AddSubview(imageView);
+            mainScrollView.AddSubview(scrollView);
 			// Perform any additional setup after loading the view, typically from a nib.
 		}
 
