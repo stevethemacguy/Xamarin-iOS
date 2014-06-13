e37d75ed4cdb4d04232594f25e18284593a688c2
Steven Dunn <sdunn@siteworx.com>
2014-06-12 18:49:16 -0700

    Added Nav Bar Items to remaining views.

--- GFS/GFS_iOS/SavedListsTableController.designer.cs
+++ GFS/GFS_iOS/SavedListsTableController.designer.cs
@@ -20,6 +20,10 @@ namespace GFS_iOS
 
 		[Outlet]
 		[GeneratedCode ("iOS Designer", "1.0")]
+		MonoTouch.UIKit.UIButton MenuB3 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
 		MonoTouch.UIKit.UITableView SavedListsUIView { get; set; }
 
 		void ReleaseDesignerOutlets ()
@@ -27,6 +31,10 @@ namespace GFS_iOS
 			if (ListCell != null) {
 				ListCell.Dispose ();
 				ListCell = null;
+			}
+			if (MenuB3 != null) {
+				MenuB3.Dispose ();
+				MenuB3 = null;
 			}
 			if (SavedListsUIView != null) {
 				SavedListsUIView.Dispose ();
