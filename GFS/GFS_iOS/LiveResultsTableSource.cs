e8cd9794876d3136c9cbea857ef6dafc24efa574
Steven Dunn <sdunn@siteworx.com>
2014-07-02 17:43:42 -0700

    Cells now use a Custom Product Cell class (based on the Xamarin tutorial for custom cells).

--- GFS/GFS_iOS/LiveResultsTableSource.cs
+++ GFS/GFS_iOS/LiveResultsTableSource.cs
@@ -10,7 +10,7 @@ namespace GFS_iOS
 	public class LiveResultsTableSource : UITableViewSource
 	{
 		private DataSource dataSource = null;
-		protected string cellIdentifier = "productCell";
+		protected NSString cellIdentifier = new NSString("productCell");
 
 		//protected List<string> tableItems; //should actually be a custom cell?
 		protected List<Product> tableItems;
@@ -27,7 +27,6 @@ namespace GFS_iOS
 				tableItems.Add(p); //Items are the actual products
 
 			this.parentController = parentController;
-
 		}
 
 		public override int RowsInSection (UITableView tableview, int section)
@@ -62,39 +61,35 @@ namespace GFS_iOS
 			Product product = tableItems[indexPath.Row]; //The cell's product
 
 			// request a recycled cell to save memory
-			UITableViewCell cell = tableView.DequeueReusableCell (cellIdentifier);
+//			UITableViewCell cell = tableView.DequeueReusableCell (cellIdentifier);
+//			// if there are no cells to reuse, create a new one
+//			if (cell == null)
+//				cell = new UITableViewCell (UITableViewCellStyle.Default, cellIdentifier);
+
+
+			// request a recycled cell to save memory
+			ProductCell cell = tableView.DequeueReusableCell (cellIdentifier) as ProductCell;
 			// if there are no cells to reuse, create a new one
 			if (cell == null)
-				cell = new UITableViewCell (UITableViewCellStyle.Default, cellIdentifier);
+				cell = new ProductCell (cellIdentifier);
+
+			//Create (or update) the cell using the Product's title, price, and image url
+			cell.UpdateCell (product.getTitle(), product.getPrice(), product.getImageFileName());
 
 			//Add a Saved List to the DataSource, using the cell row name
 			//dataSource.addSavedList(tableItems [indexPath.Row]); //Add to the static list of saved lists
+//
+//
+//			UILabel name = new UILabel();
+//			name.Frame = new RectangleF(90, 20, 150, 50);
+//			//name.Lines = 2;
+//			name.Text = product.getTitle();
+//			cell.Add(name);
 
-			//use the real image url
-			String urlString = product.getImageFileName();
-			//Don't create the image if there is none.
-			Console.WriteLine(urlString);
-			if (urlString != "")
-			{
-				NSUrl url = new NSUrl(urlString);
-				NSData data = NSData.FromUrl(url);
-				UIImage image = UIImage.LoadFromData(data); 
-				UIImageView imageView = new UIImageView(image);
-				imageView.Frame = new RectangleF(20, 20, 60, 50);
-				cell.Add(imageView);
-			}
-
-
-			UILabel name = new UILabel();
-			name.Frame = new RectangleF(90, 20, 150, 50);
-			name.Lines = 2;
-			name.Text = product.getTitle();
-			cell.Add(name);
-
-			UILabel price = new UILabel();
-			price.Frame = new RectangleF(90, 80, 100, 20);
-			price.Text = product.getPrice();
-			cell.Add(price);
+//			UILabel price = new UILabel();
+//			price.Frame = new RectangleF(90, 80, 100, 20);
+//			price.Text = product.getPrice();
+//			cell.Add(price);
 
 			return cell;
 		}
---
+++ GFS/GFS_iOS/ProductCell.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+using System.Drawing;
+
+namespace GFS_iOS
+{
+	//A custom cell to be used by the LiveResultsTable
+	public class ProductCell : UITableViewCell
+	{
+			UILabel headingLabel, subheadingLabel;
+			UIImageView imageView;
+			public ProductCell (NSString cellId) : base (UITableViewCellStyle.Default, cellId)
+			{
+				SelectionStyle = UITableViewCellSelectionStyle.Gray;
+				ContentView.BackgroundColor = UIColor.FromRGB (218, 255, 127);
+				imageView = new UIImageView();
+				headingLabel = new UILabel () {
+					Font = UIFont.FromName("Cochin-BoldItalic", 22f),
+					TextColor = UIColor.FromRGB (127, 51, 0),
+					BackgroundColor = UIColor.Clear
+				};
+				subheadingLabel = new UILabel () {
+					Font = UIFont.FromName("AmericanTypewriter", 12f),
+					TextColor = UIColor.FromRGB (38, 127, 0),
+					TextAlignment = UITextAlignment.Center,
+					BackgroundColor = UIColor.Clear
+				};
+				ContentView.Add (headingLabel);
+				ContentView.Add (subheadingLabel);
+				ContentView.Add (imageView);
+			}
+			//Get this cell it's values
+			public void UpdateCell (string caption, string subtitle, String urlString)
+			{
+				//Create the image from the urlString
+				
+		
+				//Don't create the image if there is none.
+				if (urlString != "")
+				{
+					//Create a url from the string and use it with an NSData object
+					NSData data = NSData.FromUrl(new NSUrl(urlString));
+
+					//Create a UIimage using the url to load the image
+					imageView.Image = UIImage.LoadFromData(data);
+				}
+				
+				headingLabel.Text = caption;
+				subheadingLabel.Text = subtitle;
+			}
+
+			//Position the views in the cell
+			public override void LayoutSubviews()
+			{
+				base.LayoutSubviews ();
+				imageView.Frame = new RectangleF(ContentView.Bounds.Width - 63, 5, 33, 33);
+				headingLabel.Frame = new RectangleF(5, 4, ContentView.Bounds.Width - 63, 25);
+				subheadingLabel.Frame = new RectangleF(100, 18, 100, 20);
+			}
+	}
+}
+
