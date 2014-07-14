42b3a0c88f03d54e5f305e5287eaf16abc931a42
“Steven <sdunn@siteworx.com>
2014-07-14 15:40:53 -0700

    Added NewtonJsonReader and a reference to the existing item.

---
+++ GFS/Core/SVC/NewtonJsonReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swx.B2B.Ecom.SVC
+{
+    class NewtonJsonReader
+    {
+    }
+}
