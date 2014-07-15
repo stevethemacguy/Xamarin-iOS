e3e71247460e5e5a12b7d67ae9dc8d82e4443a03
Steven Dunn <sdunn@siteworx.com>
2014-07-15 16:39:37 -0700

    Added commits. Change the "List" class to "SavedList" to avoid conflicts with System "List" Type.

--- GFS/Core/BL/Entities/JsonSuggestion.cs
+++ GFS/Core/BL/Entities/JsonSuggestion.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+//A JsonSuggestion represents a single suggested word (a String). 
+//Having a whole class for this is overkill, but currently the NewtonJsonReader always instaniates to objects when parsing Json
 namespace Swx.B2B.Ecom.BL.Entities
 {
 	public class JsonSuggestion
